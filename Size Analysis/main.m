64e9a90a8967dc58631ffca212f55f8a17990eae
Varsha Hanji <varsha.hanji@c02wt2h7htdf.local>
2018-08-17 15:59:01 -0700

    Initial Commit

---
+++ Size Analysis/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Size Analysis
+//
+//  Created by Varsha Hanji on 8/17/18.
+//  Copyright © 2018 Varsha Hanji. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
